03c7c85ca3ab10db9bd8c46757e4d5f71541913a
Katina Papadakis <katina.papadakis@yahoo.com>
2014-12-05 12:19:35 -0800

    Plot 3 legend issues...

--- figure/plot3.R
+++ figure/plot3.R
@@ -22,7 +22,7 @@ points(power$datetime,power$Sub_metering_3,type="l",
        col="blue")
 legend("topright",bty="n",legend=c('Sub_metering_1',
        'Sub_metering_2','Sub_metering_3'),col=c('black','red',
-        'blue'),cex=0.8,lwd=1)
+        'blue'),cex=0.6,lwd=1)
 dev.copy(png, file="plot3.png")
 dev.off()
 
