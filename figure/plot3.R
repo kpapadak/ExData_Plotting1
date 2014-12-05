0668663d81834e73fb964e5c5495174915165359
Katina Papadakis <katina.papadakis@yahoo.com>
2014-12-05 12:16:59 -0800

    Fixed plot3 legend.

--- figure/plot3.R
+++ figure/plot3.R
@@ -20,6 +20,9 @@ points(power$datetime,power$Sub_metering_2,type="l",
        col="red")
 points(power$datetime,power$Sub_metering_3,type="l",
        col="blue")
+legend("topright",bty="n",legend=c('Sub_metering_1',
+       'Sub_metering_2','Sub_metering_3'),col=c('black','red',
+        'blue'),cex=0.8,lwd=1)
 dev.copy(png, file="plot3.png")
 dev.off()
 
